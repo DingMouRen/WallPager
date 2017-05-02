6e3e67b5c2f61c59890ddd9513358267c832b510
dingmouren <1341520108@qq.com>
2017-05-02 17:28:58 +0800

    commit

--- app/src/main/java/com/dingmouren/wallpager/ui/home/MainActivity.java
+++ app/src/main/java/com/dingmouren/wallpager/ui/home/MainActivity.java
@@ -1,9 +1,12 @@
 package com.dingmouren.wallpager.ui.home;
 
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
 
 import com.dingmouren.wallpager.R;
 import com.dingmouren.wallpager.base.BaseActivity;
@@ -15,6 +18,7 @@ import butterknife.ButterKnife;
 public class MainActivity extends BaseActivity {
 
     @BindView(R.id.toolbar)  Toolbar mToolbar;
+    @BindView(R.id.drawer_layout) DrawerLayout mDrawerLayout;
 
     private FragmentManager mFragmentManager;
 
@@ -33,6 +37,14 @@ public class MainActivity extends BaseActivity {
     public void initView() {
         mToolbar.setTitle("壁纸");
         setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
         mFragmentManager.beginTransaction().add(R.id.frame_layout,new HomeFragment()).commit();
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
     }
 }
